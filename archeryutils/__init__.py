e10ab27d310abfad4f8ca232f07fd57f41d8ba8d
Jack Atkinson <jack.atkinson1000@gmail.com>
2022-12-21 20:15:46 +0000

    updated init to import classifications on install

--- archeryutils/__init__.py
+++ archeryutils/__init__.py
@@ -1,9 +1,11 @@
 from archeryutils import rounds, targets
 from archeryutils.handicaps import handicap_equations, handicap_functions
+from archeryutils.classifications import classifications
 
 __all__ = [
     "rounds",
     "targets",
     "handicap_equations",
     "handicap_functions",
+    "classifications",
 ]
