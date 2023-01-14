df849fa909288a18bc3801b79b20a24be407eca1
Jack Atkinson <jack.atkinson1000@gmail.com>
2023-01-14 18:46:59 +0000

    Added AGB field classifications.

--- archeryutils/classifications/classifications.py
+++ archeryutils/classifications/classifications.py
@@ -201,7 +201,7 @@ def _make_AGB_outdoor_classification_dict():
                     if i <= 3:
                         # All MB and B1 require max distance for everyone:
                         min_dists[i, :] = padded_dists[
-                            max_dist_index: max_dist_index + 3
+                            max_dist_index : max_dist_index + 3
                         ]
                     else:
                         try:
@@ -217,13 +217,13 @@ def _make_AGB_outdoor_classification_dict():
                                 "under16",
                             ]:
                                 min_dists[i, :] = padded_dists[
-                                    max_dist_index + i - 3: max_dist_index + i
+                                    max_dist_index + i - 3 : max_dist_index + i
                                 ]
                             # All other categories require max dist for B1 and B2 then step down
                             else:
                                 try:
                                     min_dists[i, :] = padded_dists[
-                                        max_dist_index + i - 4: max_dist_index + i - 1
+                                        max_dist_index + i - 4 : max_dist_index + i - 1
                                     ]
                                 except ValueError:
                                     # Distances stack at the bottom end
@@ -300,8 +300,7 @@ def _make_AGB_outdoor_classification_dict():
 def _make_AGB_indoor_classification_dict():
     """
     Subroutine to generate a dictionary of dictionaries providing handicaps for
-    each classification band :and a list prestige rounds for each category from
-    data files.
+    each classification band
 
     Parameters
     ----------
@@ -343,11 +342,158 @@ def _make_AGB_indoor_classification_dict():
     return classification_dict
 
 
+def _make_AGB_field_classification_dict():
+    """
+    Subroutine to generate a dictionary of dictionaries providing handicaps for
+    each classification band
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    classification_dict : dict of str : dict of str: list
+        dictionary indexed on group name (e.g 'adult_female_recurve')
+        containing list of scores associated with each classification
+
+    References
+    ----------
+    ArcheryGB 2023 Rules of Shooting
+    ArcheryGB Shooting Administrative Procedures - SAP7 (2023)
+    """
+
+    AGB_field_classes = [
+        "Grand Master Bowman",
+        "Master Bowman",
+        "Bowman",
+        "1st Class",
+        "2nd Class",
+        "3rd Class",
+    ]
+
+    # Generate dict of classifications
+    # for both bowstyles, for both genders
+    classification_dict = {}
+    classification_dict[get_groupname("Compound", "Male", "Adult")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [393, 377, 344, 312, 279, 247],
+    }
+    classification_dict[get_groupname("Compound", "Female", "Adult")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [376, 361, 330, 299, 268, 237],
+    }
+    classification_dict[get_groupname("Recurve", "Male", "Adult")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [338, 317, 288, 260, 231, 203],
+    }
+    classification_dict[get_groupname("Recurve", "Female", "Adult")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [322, 302, 275, 247, 220, 193],
+    }
+    classification_dict[get_groupname("Barebow", "Male", "Adult")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [328, 307, 279, 252, 224, 197],
+    }
+    classification_dict[get_groupname("Barebow", "Female", "Adult")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [303, 284, 258, 233, 207, 182],
+    }
+    classification_dict[get_groupname("Longbow", "Male", "Adult")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [201, 188, 171, 155, 137, 121],
+    }
+    classification_dict[get_groupname("Longbow", "Female", "Adult")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [303, 284, 258, 233, 207, 182],
+    }
+    classification_dict[get_groupname("Traditional", "Male", "Adult")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [262, 245, 223, 202, 178, 157],
+    }
+    classification_dict[get_groupname("Traditional", "Female", "Adult")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [197, 184, 167, 152, 134, 118],
+    }
+    classification_dict[get_groupname("Flatbow", "Male", "Adult")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [262, 245, 223, 202, 178, 157],
+    }
+    classification_dict[get_groupname("Flatbow", "Female", "Adult")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [197, 184, 167, 152, 134, 118],
+    }
+
+    # Juniors
+    classification_dict[get_groupname("Compound", "Male", "Under 18")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [385, 369, 337, 306, 273, 242],
+    }
+
+    classification_dict[get_groupname("Compound", "Female", "Under 18")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [357, 343, 314, 284, 255, 225],
+    }
+
+    classification_dict[get_groupname("Recurve", "Male", "Under 18")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [311, 292, 265, 239, 213, 187],
+    }
+
+    classification_dict[get_groupname("Recurve", "Female", "Under 18")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [280, 263, 239, 215, 191, 168],
+    }
+
+    classification_dict[get_groupname("Barebow", "Male", "Under 18")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [298, 279, 254, 229, 204, 179],
+    }
+
+    classification_dict[get_groupname("Barebow", "Female", "Under 18")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [251, 236, 214, 193, 172, 151],
+    }
+
+    classification_dict[get_groupname("Longbow", "Male", "Under 18")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [161, 150, 137, 124, 109, 96],
+    }
+
+    classification_dict[get_groupname("Longbow", "Female", "Under 18")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [122, 114, 103, 94, 83, 73],
+    }
+
+    classification_dict[get_groupname("Traditional", "Male", "Under 18")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [210, 196, 178, 161, 143, 126],
+    }
+
+    classification_dict[get_groupname("Traditional", "Female", "Under 18")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [158, 147, 134, 121, 107, 95],
+    }
+
+    classification_dict[get_groupname("Flatbow", "Male", "Under 18")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [210, 196, 178, 161, 143, 126],
+    }
+
+    classification_dict[get_groupname("Flatbow", "Female", "Under 18")] = {
+        "classes": AGB_field_classes,
+        "class_scores": [158, 147, 134, 121, 107, 95],
+    }
+
+    return classification_dict
+
+
 AGB_outdoor_classifications = _make_AGB_outdoor_classification_dict()
 AGB_indoor_classifications = _make_AGB_indoor_classification_dict()
+AGB_field_classifications = _make_AGB_field_classification_dict()
 
 del _make_AGB_outdoor_classification_dict
 del _make_AGB_indoor_classification_dict
+del _make_AGB_field_classification_dict
 
 
 def calculate_AGB_outdoor_classification(roundname, score, bowstyle, gender, age_group):
@@ -390,6 +536,9 @@ def calculate_AGB_outdoor_classification(roundname, score, bowstyle, gender, age
             "WA_outdoor.json",
         ]
     )
+
+    if bowstyle.lower() in ["traditional", "flatbow"]:
+        bowstyle = "Barebow"
 
     groupname = get_groupname(bowstyle, gender, age_group)
     group_data = AGB_outdoor_classifications[groupname]
@@ -673,6 +822,124 @@ def AGB_indoor_classification_scores(
         )
 
     return class_scores
+
+
+def calculate_AGB_field_classification(roundname, score, bowstyle, gender, age_group):
+    """
+    Subroutine to calculate a classification from a score given suitable inputs
+
+    Parameters
+    ----------
+    roundname : str
+        name of round shot as given by 'codename' in json
+    score : int
+        numerical score on the round to calculate classification for
+    bowstyle : str
+        archer's bowstyle under AGB outdoor target rules
+    gender : str
+        archer's gender under AGB outdoor target rules
+    age_group : str
+        archer's age group under AGB outdoor target rules
+
+    Returns
+    -------
+    classification_from_score : str
+        the classification appropriate for this score
+
+    References
+    ----------
+    ArcheryGB 2023 Rules of Shooting
+    ArcheryGB Shooting Administrative Procedures - SAP7 (2023)
+    """
+
+    # TODO: Need routines to sanitise/deal with variety of user inputs
+
+    # TODO: Should this be defined outside the function to reduce I/O or does
+    #   it have no effect?
+    all_field_rounds = rounds.read_json_to_round_dict(
+        [
+            "WA_field.json",
+        ]
+    )
+
+    # deal with reduced categories:
+    if age_group.lower().replace(" ", "") in ["adult", "50+", "under21"]:
+        age_group = "Adult"
+    else:
+        age_group = "Under 18"
+
+    groupname = get_groupname(bowstyle, gender, age_group)
+
+    # Get scores required on this round for each classification
+    group_data = AGB_field_classifications[groupname]
+
+    # Check Round is appropriate:
+    if bowstyle.lower() in ["compound", "recurve"] and roundname != "wa_field_24_red":
+        return "unclassified"
+    if (
+        bowstyle.lower() in ["barebow", "longbow", "traditional", "flatbow"]
+        and roundname != "wa_field_24_blue"
+    ):
+        return "unclassified"
+    else:
+        # What is the highest classification this score gets?
+        class_scores = dict(zip(group_data["classes"], group_data["class_scores"]))
+        for item in class_scores:
+            if class_scores[item] > score:
+                pass
+            else:
+                return item
+
+        # if lower than 3rd class score return "UC"
+        return "unclassified"
+
+
+def AGB_field_classification_scores(roundname, bowstyle, gender, age_group):
+    """
+    Subroutine to calculate classification scores for a specific category and round
+    Appropriate ArcheryGB age groups and classifications
+
+    Parameters
+    ----------
+    roundname : str
+        name of round shot as given by 'codename' in json
+    bowstyle : str
+        archer's bowstyle under AGB outdoor target rules
+    gender : str
+        archer's gender under AGB outdoor target rules
+    age_group : str
+        archer's age group under AGB outdoor target rules
+
+    Returns
+    -------
+    classification_scores : ndarray
+        abbreviation of the classification appropriate for this score
+
+    References
+    ----------
+    ArcheryGB Rules of Shooting
+    ArcheryGB Shooting Administrative Procedures - SAP7
+    """
+
+    # TODO: Should this be defined outside the function to reduce I/O or does
+    #   it have no effect?
+    all_field_rounds = rounds.read_json_to_round_dict(
+        [
+            "WA_field.json",
+        ]
+    )
+
+    # deal with reduced categories:
+    if age_group.lower().replace(" ", "") in ["adult", "50+", "under21"]:
+        age_group = "Adult"
+    else:
+        age_group = "Under 18"
+
+    groupname = get_groupname(bowstyle, gender, age_group)
+    group_data = AGB_field_classifications[groupname]
+
+    # Get scores required on this round for each classification
+    return group_data["class_scores"]
 
 
 if __name__ == "__main__":
