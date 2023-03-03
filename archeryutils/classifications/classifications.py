1e7c4e9f335cf29bfe95ba0b7084b6dcb60a6013
Jack Atkinson <jack.atkinson1000@gmail.com>
2023-03-03 14:42:34 +0000

    Added Marked/Unmarked/Mixed WA field rounds.

--- archeryutils/classifications/classifications.py
+++ archeryutils/classifications/classifications.py
@@ -874,12 +874,12 @@ def calculate_AGB_field_classification(roundname, score, bowstyle, gender, age_g
     group_data = AGB_field_classifications[groupname]
 
     # Check Round is appropriate:
-    if bowstyle.lower() in ["compound", "recurve"] and roundname != "wa_field_24_red":
+    # Sighted can have any Red 24, unsightes can have any blue 24
+    if bowstyle.lower() in ["compound", "recurve"] and "wa_field_24_red_" not in roundname:
         return "unclassified"
     if (
         bowstyle.lower() in ["barebow", "longbow", "traditional", "flatbow"]
-        and roundname != "wa_field_24_blue"
-    ):
+        and "wa_field_24_blue_" not in roundname):
         return "unclassified"
     else:
         # What is the highest classification this score gets?
