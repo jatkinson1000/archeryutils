32838c411b7a5fbd649a776aaff899d9d91c3935
Tom Hall <thall-tom@hotmail.co.uk>
2024-01-12 23:12:21 +0000

    Document Length conversion class

--- archeryutils/constants.py
+++ archeryutils/constants.py
@@ -49,14 +49,38 @@ _INCH_ALIASES = {
     "inches",
 }
 
-_ALIASES = dict(
-    yard = _YARD_ALIASES,
-    metre = _METRE_ALIASES,
-    cm = _CM_ALIASES,
-    inch = _INCH_ALIASES,
-)
+_ALIASES = {
+    "yard" : _YARD_ALIASES,
+    "metre" : _METRE_ALIASES,
+    "cm" : _CM_ALIASES,
+    "inch" : _INCH_ALIASES,
+}
 
 class Length:
+    """
+    Utility class for Length unit conversions
+
+    Contains common abbreviations, pluralisations and capitilizations for supported
+    units as sets to allow easy membership checks in combination.
+    Methods for conversions to and from metres are provided as classmethods.
+
+    Attributes
+    ----------
+    yard : set[str]
+    metre : set[str]
+    cm: set[str]
+    inch: set[str]
+
+    Methods
+    -------
+    to_metres()
+        Convert distance in any supported unit to metres
+    from_metres()
+        Convert distance in metres to any supported unit
+    definitive_name()
+        Convert any string alias representing a distance unit to a single version.
+    """
+
     yard = _YARD_ALIASES
     metre = _METRE_ALIASES
     cm = _CM_ALIASES
@@ -76,12 +100,15 @@ class Length:
 
     @classmethod
     def to_metres(cls, value: float, unit: str) -> float:
+        """Convert value in metres to given unit"""
         return cls._conversions[unit] * value
 
     @classmethod
     def from_metres(cls, metre_value: float, unit: str) -> float:
+        """Convert value in given unit to metres"""
         return metre_value / cls._conversions[unit]
 
     @classmethod
     def definitive_name(cls, alias: str) -> str:
+        """Convert alias for unit into a single definied name set in constants"""
         return cls._reversed[alias]
