3fa83983d96f45cee7fe224a86a7e98c56ac329a
Jack Atkinson <jwa34@cam.ac.uk>
2024-01-27 19:40:30 +0000

    Update top level namespace to remove redundant target and round modules.

--- archeryutils/__init__.py
+++ archeryutils/__init__.py
@@ -1,20 +1,16 @@
 """Package providing code for various archery utilities."""
 
-from archeryutils import rounds, targets
+from archeryutils.targets import Target
+from archeryutils.rounds import Pass, Round
 from archeryutils.handicaps import handicap_equations, handicap_functions
 from archeryutils import classifications
 
-from archeryutils.targets import Target
-from archeryutils.rounds import Pass, Round
-
 
 __all__ = [
-    "rounds",
-    "targets",
-    "handicap_equations",
-    "handicap_functions",
-    "classifications",
     "Target",
     "Pass",
     "Round",
+    "handicap_equations",
+    "handicap_functions",
+    "classifications",
 ]
