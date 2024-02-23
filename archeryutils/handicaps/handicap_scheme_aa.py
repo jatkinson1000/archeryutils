9cdcbf377cd264ddb2aaed0ffd7d8658bee09bbe
Jack Atkinson <jwa34@cam.ac.uk>
2024-02-23 09:36:49 +0000

    Refactor handicaps code into Class structure
    Co-authored-by Liam Pattinson <liam.pattinson@york.ac.uk>

---
+++ archeryutils/handicaps/handicap_scheme_aa.py
@@ -0,0 +1,296 @@
+"""Code for handicap calculations using Archery Australia handicap schemes.
+
+Extended Summary
+----------------
+Code to calculate information using a number of schemes:
+
+- Original Archery Australia (J Park)
+- Updated Archery Australia (J Park)
+
+Routine Listings
+----------------
+- HandicapAA Class
+- HandicapAA2 Class
+
+References
+----------
+- Modelling archers’ scores at different distances to quantify
+  score loss due to equipment selection and technique errors
+  Park, J (2014)
+  https://doi.org/10.1177%2F1754337114539308
+
+"""
+
+import numpy as np
+
+from .handicap_scheme import FloatArray, HandicapScheme
+
+
+class HandicapAA(HandicapScheme):
+    """
+    Class to represent the original Archery Australia scheme by J. Park.
+
+    Parameters
+    ----------
+    ang_0 : float
+        datum angle used in scheme, default = 1.0e-3,
+    k0 : float
+        handicap offset to set scratch point, default= 2.37,
+    ks : float
+        change with each handicap step as a geometric progression, default= 0.027,
+    kd : float
+        distance scaling factor, default= 0.004,
+
+    Attributes
+    ----------
+    name : str="AA"
+        The name of the handicap scheme.
+    arw_d_out : float
+        diameter of an outdoor arrow: 5.0e-3 [metres]
+    arw_d_in: float
+        diameter of an indoor arrow 9.3e-3 [metres]
+
+    Methods
+    -------
+    sigma_t(handicap, dist)
+        Calculate angular deviation for given handicap and distance.
+    sigma_r(handicap, dist)
+        Calculate radial deviation for a given handicap and distance.
+    arrow_score(target, handicap, arw_d=None)
+        Calculate the average arrow score for a given target and handicap.
+    score_for_passes(rnd, handicap, arw_d=None, rounded_score=True)
+        Calculate the expected score for all passes in a round for a given handicap.
+    score_for_round(rnd, handicap, arw_d=None, rounded_score=True)
+        Calculate the expected score for a round for a given handicap.
+    handicap_from_score(score,rnd, arw_d=None, int_prec=False)
+        Calculate the handicap for a given score on a given round.
+
+    Warnings
+    --------
+    Using non-default values for the kwargs may produce results that
+    do not match the official values for this scheme.
+    Only change if you are experimenting and know what you are doing!
+
+    See Also
+    --------
+    HandicapScheme : The base class for a handicap scheme from which this is subclassed.
+
+    """
+
+    def __init__(
+        self,
+        ang_0: float = 1.0e-3,
+        k0: float = 2.37,
+        ks: float = 0.027,
+        kd: float = 0.004,
+    ):
+        self.params = {
+            "ang_0": ang_0,  # Baseline angle used for group size 1.0 [millirad].
+            "k0": k0,  # Offset required to set handicap 100 at desired score.
+            "ks": ks,  # Change with each step of geometric progression.
+            "kd": kd,  # Distance scaling factor [1/metres].
+        }
+
+        self.arw_d_out = 5.0e-3
+
+        self.name = "AA"
+        self.desc_scale = False
+
+    def sigma_t(self, handicap: FloatArray, dist: float) -> FloatArray:
+        """Calculate angular deviation for given handicap and distance.
+
+        Parameters
+        ----------
+        handicap : FloatArray
+            handicap to calculate sigma_t at
+        dist : float
+            distance to target [metres]
+
+        Returns
+        -------
+        sig_t : FloatArray
+            angular deviation [rad]
+
+        Notes
+        -----
+        This is the key part of this scheme.
+        The values are taken from [1]_
+
+        References
+        ----------
+        .. [1] Park, J. L. (2014). "Modelling archers’ scores at different distances
+           to quantify score loss due to equipment selection and technique errors."
+           Proceedings of the Institution of Mechanical Engineers,
+           Part P: Journal of Sports Engineering and Technology, 228(4), 250-258.
+           DOI: 10.1177/175433711453930
+
+        Examples
+        --------
+        Angular deviation at a distance of 25m, using the AGBold handicap system at a
+        handicap of 10 can be calculated with:
+
+        >>> import archeryutils.handicaps as hc
+        >>> aa_scheme = hc.handicap_scheme("AA")
+        >>> aa_scheme.sigma_t(10.0, 25.0)
+        0.012763296491500004
+
+        It can also be passed an array of handicaps:
+
+        >>> import numpy as np
+        >>> aa_scheme.sigma_t(np.array([10.0, 50.0, 100.0]), 25.0)
+        array([0.0127633 , 0.00433436, 0.00112364])
+
+        """
+        # Factor of sqrt(2) to deal with factor of 2 in differing definitions of sigma
+        # between AGB and AA
+        # Required so code elsewhere is unchanged
+        # Factor of 1.0e-3 due to AA algorithm specifying sigma t in milliradians, so
+        # convert to rad
+        return (
+            np.sqrt(2.0)
+            * self.params["ang_0"]
+            * np.exp(
+                self.params["k0"]
+                - self.params["ks"] * handicap
+                + self.params["kd"] * dist
+            )
+        )
+
+
+class HandicapAA2(HandicapScheme):
+    """
+    Class to represent the updated (2014) Archery Australia scheme by J. Park.
+
+    Parameters
+    ----------
+    ang_0 : float
+        datum angle used in scheme, default = 1.0e-3,
+    k0 : float
+        handicap offset to set scratch point, default= 2.57,
+    ks : float
+        change with each handicap step as a geometric progression, default= 0.027,
+    f1 : float
+        'linear' scaling factor, default= 0.815,
+    f2 : float
+        'quadratic' scaling factor, default= 0.185,
+    d0 : float
+        normalisation distance, default = 50.0,
+
+    Attributes
+    ----------
+    name : str="AA2"
+        The name of the handicap scheme.
+    arw_d_out : float
+        diameter of an outdoor arrow: 5.0e-3 [metres]
+    arw_d_in: float
+        diameter of an indoor arrow 9.3e-3 [metres]
+
+    Methods
+    -------
+    sigma_t(handicap, dist)
+        Calculate angular deviation for given handicap and distance.
+    sigma_r(handicap, dist)
+        Calculate radial deviation for a given handicap and distance.
+    arrow_score(target, handicap, arw_d=None)
+        Calculate the average arrow score for a given target and handicap.
+    score_for_passes(rnd, handicap, arw_d=None, rounded_score=True)
+        Calculate the expected score for all passes in a round for a given handicap.
+    score_for_round(rnd, handicap, arw_d=None, rounded_score=True)
+        Calculate the expected score for a round for a given handicap.
+    handicap_from_score(score,rnd, arw_d=None, int_prec=False)
+        Calculate the handicap for a given score on a given round.
+
+    Warnings
+    --------
+    Using non-default values for the kwargs may produce results that
+    do not match the official values for this scheme.
+    Only change if you are experimenting and know what you are doing!
+
+    See Also
+    --------
+    HandicapScheme : The base class for a handicap scheme from which this is subclassed.
+
+    """
+
+    def __init__(
+        self,
+        ang_0: float = 1.0e-3,
+        k0: float = 2.57,
+        ks: float = 0.027,
+        f1: float = 0.815,
+        f2: float = 0.185,
+        d0: float = 50.0,
+    ):
+        # two too many arguments, but all are hc-scheme params => disable
+        # pylint: disable=too-many-arguments
+
+        self.params = {
+            "ang_0": ang_0,
+            "k0": k0,  # Offset required to set handicap 100 at desired score.
+            "ks": ks,  # Change with each step of geometric progression.
+            "f1": f1,  # 'Linear' scaling factor.
+            "f2": f2,  # 'Quadratic' scaling factor.
+            "d0": d0,  # Normalisation distance [metres].
+        }
+
+        self.arw_d_out = 5.0e-3
+
+        self.name = "AA2"
+        self.desc_scale = False
+
+    def sigma_t(self, handicap: FloatArray, dist: float) -> FloatArray:
+        """Calculate angular deviation for given handicap and distance.
+
+        Parameters
+        ----------
+        handicap : FloatArray
+            handicap to calculate sigma_t at
+        dist : float
+            distance to target [metres]
+
+        Returns
+        -------
+        sig_t : FloatArray
+            angular deviation [rad]
+
+        Notes
+        -----
+        This is the key part of this scheme.
+        The values are taken from [1]_
+
+        References
+        ----------
+        .. [1] Park, J. L. (2014). "Modelling archers’ scores at different distances
+           to quantify score loss due to equipment selection and technique errors."
+           Proceedings of the Institution of Mechanical Engineers,
+           Part P: Journal of Sports Engineering and Technology, 228(4), 250-258.
+           DOI: 10.1177/175433711453930
+
+        Examples
+        --------
+        Angular deviation at a distance of 25m, using the AGBold handicap system at a
+        handicap of 10 can be calculated with:
+
+        >>> import archeryutils.handicaps as hc
+        >>> aa2_scheme = hc.handicap_scheme("AA2")
+        >>> aa2_scheme.sigma_t(10.0, 25.0)
+        0.012800853871823342
+
+        It can also be passed an array of handicaps:
+
+        >>> import numpy as np
+        >>> aa2_scheme.sigma_t(np.array([10.0, 50.0, 100.0]), 25.0)
+        array([0.01280085, 0.00434711, 0.00112695])
+
+        """
+        # Factor of sqrt(2) to deal with factor of 2 in differing definitions of sigma
+        # between AGB and AA
+        # Required so code elsewhere is unchanged
+        # Factor of 1.0e-3 due to AA algorithm specifying sigma t in milliradians, so
+        # convert to rad
+        return (
+            np.sqrt(2.0)
+            * self.params["ang_0"]
+            * np.exp(self.params["k0"] - self.params["ks"] * handicap)
+            * (self.params["f1"] + self.params["f2"] * dist / self.params["d0"])
+        )
---
+++ archeryutils/handicaps/handicap_scheme_agb.py
@@ -0,0 +1,278 @@
+"""Code for handicap calculations using Archery GB handicap schemes.
+
+Extended Summary
+----------------
+Code to calculate information using a number of schemes:
+
+- New Archery GB (J Atkinson, 2023)
+- Old Archery GB (D Lane)
+
+Routine Listings
+----------------
+- HandicapAGB Class
+- HandicapAGBold Class
+
+References
+----------
+- New AGB: Atkinson, J
+- Old AGB: The construction of the graduated handicap tables for target
+  archery
+  Lane, D (2013)
+  https://www.jackatkinson.net/files/Handicap_Tables_2013.pdf
+
+"""
+
+import numpy as np
+
+from .handicap_scheme import FloatArray, HandicapScheme
+
+
+class HandicapAGB(HandicapScheme):
+    """
+    Class to represent the Archery GB handicap scheme.
+
+    This class represents the 2023 handicap scheme by Jack Atkinson.
+
+    Parameters
+    ----------
+    datum : float
+        offset to set scratch point, default = 6.0,
+    step : float
+        percentage change in deviation per handicap step, default = 3.5,
+    ang_0 : float
+        datum angle used in scheme, default = 5.0e-4,
+    kd : float
+        factor controlling excess dispersion scaling with distance, default = 0.00365,
+
+    Attributes
+    ----------
+    name : str="AGB"
+        The name of the handicap scheme.
+    arw_d_out : float
+        diameter of an outdoor arrow: 5.5e-3 [metres]
+    arw_d_in: float
+        diameter of an indoor arrow 9.3e-3 [metres]
+
+    Methods
+    -------
+    sigma_t(handicap, dist)
+        Calculate angular deviation for given handicap and distance.
+    sigma_r(handicap, dist)
+        Calculate radial deviation for a given handicap and distance.
+    arrow_score(target, handicap, arw_d=None)
+        Calculate the average arrow score for a given target and handicap.
+    score_for_passes(rnd, handicap, arw_d=None, rounded_score=True)
+        Calculate the expected score for all passes in a round for a given handicap.
+    score_for_round(rnd, handicap, arw_d=None, rounded_score=True)
+        Calculate the expected score for a round for a given handicap.
+    handicap_from_score(score,rnd, arw_d=None, int_prec=False)
+        Calculate the handicap for a given score on a given round.
+
+    Warnings
+    --------
+    Using non-default values for the kwargs may produce results that
+    do not match the official values for this scheme.
+    Only change if you are experimenting and know what you are doing!
+
+    See Also
+    --------
+    HandicapScheme : The base class for a handicap scheme from which this is subclassed.
+
+    """
+
+    def __init__(
+        self,
+        datum: float = 6.0,
+        step: float = 3.5,
+        ang_0: float = 5.0e-4,
+        kd: float = 0.00365,
+    ):
+        self.params = {
+            "datum": datum,
+            "step": step,
+            "ang_0": ang_0,
+            "kd": kd,
+        }
+
+        self.name = "AGB"
+
+    def sigma_t(self, handicap: FloatArray, dist: float) -> FloatArray:
+        """Calculate angular deviation for given handicap and distance.
+
+        Parameters
+        ----------
+        handicap : FloatArray
+            handicap to calculate sigma_t at
+        dist : float
+            distance to target [metres]
+
+        Returns
+        -------
+        sig_t : FloatArray
+            angular deviation [rad]
+
+        Examples
+        --------
+        Angular deviation at a distance of 25m, using the AGB handicap system at a
+        handicap of 10 can be calculated with:
+
+        >>> import archeryutils.handicaps as hc
+        >>> agb_scheme = hc.handicap_scheme("AGB")
+        >>> agb_scheme.sigma_t(10.0, 25.0)
+        0.0009498280098103058
+
+        It can also be passed an array of handicaps:
+
+        >>> import numpy as np
+        >>> agb_scheme.sigma_t(np.array([10.0, 50.0, 100.0]), 25.0)
+        array([0.00094983, 0.00376062, 0.02100276])
+
+        """
+        return (
+            self.params["ang_0"]
+            * ((1.0 + self.params["step"] / 100.0) ** (handicap + self.params["datum"]))
+            * np.exp(self.params["kd"] * dist)
+        )
+
+    # Override rounding method for AGB to always round up to next highest score.
+    @staticmethod
+    def _rounded_score(score):
+        return np.ceil(score)
+
+
+class HandicapAGBold(HandicapScheme):
+    """
+    Class to represent the old (pre-2023) Archery GB handicap scheme by D. Lane.
+
+    Parameters
+    ----------
+    datum : float
+        offset to set scratch point, default = 12.9,
+    step : float
+        percentage change in deviation per handicap step, default = 3.6,
+    ang_0 : float
+        datum angle used in scheme, default = 5.0e-4,
+    k1 : float
+        constant k1 in the handicap equation, default = 1.429e-6,
+    k2 : float
+        constant k2 in the handicap equation, default = 1.07,
+    k3 : float
+        constant k3 in the handicap equation, default = 4.3,
+    p1 : float
+        exponent of distance scaling, default = 2.0,
+
+    Attributes
+    ----------
+    name : str="AGBold"
+        The name of the handicap scheme.
+    arw_d_out : float
+        diameter of an outdoor arrow: 7.14e-3 [metres]
+    arw_d_in: float
+        diameter of an indoor arrow 7.14e-3 [metres]
+
+    Methods
+    -------
+    sigma_t(handicap, dist)
+        Calculate angular deviation for given handicap and distance.
+    sigma_r(handicap, dist)
+        Calculate radial deviation for a given handicap and distance.
+    arrow_score(target, handicap, arw_d=None)
+        Calculate the average arrow score for a given target and handicap.
+    score_for_passes(rnd, handicap, arw_d=None, rounded_score=True)
+        Calculate the expected score for all passes in a round for a given handicap.
+    score_for_round(rnd, handicap, arw_d=None, rounded_score=True)
+        Calculate the expected score for a round for a given handicap.
+    handicap_from_score(score,rnd, arw_d=None, int_prec=False)
+        Calculate the handicap for a given score on a given round.
+
+    Warnings
+    --------
+    Using non-default values for the kwargs may produce results that
+    do not match the official values for this scheme.
+    Only change if you are experimenting and know what you are doing!
+
+    See Also
+    --------
+    HandicapScheme : The base class for a handicap scheme from which this is subclassed.
+
+    """
+
+    def __init__(
+        self,
+        datum: float = 12.9,
+        step: float = 3.6,
+        ang_0: float = 5.0e-4,
+        k1: float = 1.429e-6,
+        k2: float = 1.07,
+        k3: float = 4.3,
+        p1: float = 2.0,
+    ):
+        # three too many arguments, but all are hc-scheme params => disable
+        # pylint: disable=too-many-arguments
+
+        self.params = {
+            "datum": datum,  # Offset required to set handicap 0 at desired score.
+            "step": step,  # Percentage change in group size for each handicap step.
+            "ang_0": ang_0,  # Baseline angle used for group size 0.5 [millirad].
+            "k1": k1,  # Constant 1 used in handicap equation.
+            "k2": k2,  # Constant 2 used in handicap equation.
+            "k3": k3,  # Constant 3 used in handicap equation.
+            "p1": p1,  # Exponent of distance scaling.
+        }
+
+        self.arw_d_out = 7.14e-3
+        self.arw_d_in = 7.14e-3
+
+        self.name = "AGBold"
+
+    def sigma_t(self, handicap: FloatArray, dist: float) -> FloatArray:
+        """Calculate angular deviation for given handicap and distance.
+
+        Parameters
+        ----------
+        handicap : FloatArray
+            handicap to calculate sigma_t at
+        dist : float
+            distance to target [metres]
+
+        Returns
+        -------
+        sig_t : FloatArray
+            angular deviation [rad]
+
+        Notes
+        -----
+        This is the key part of this scheme.
+        The values are taken from [1]_
+
+        References
+        ----------
+        .. [1] Lane, D. (2013). "The Construction of the Graduated Handicap Tables
+           for Target Archery."
+
+        Examples
+        --------
+        Angular deviation at a distance of 25m, using the AGBold handicap system at a
+        handicap of 10 can be calculated with:
+
+        >>> import archeryutils.handicaps as hc
+        >>> agbold_scheme = hc.handicap_scheme("AGBold")
+        >>> agbold_scheme.sigma_t(10.0, 25.0)
+        0.001126491382794861
+
+        It can also be passed an array of handicaps:
+
+        >>> import numpy as np
+        >>> agbold_scheme.sigma_t(np.array([10.0, 50.0, 100.0]), 25.0)
+        array([0.00112649, 0.00478762, 0.05520862])
+
+        """
+        k_factor = self.params["k1"] * self.params["k2"] ** (
+            handicap + self.params["k3"]
+        )
+        f_factor = 1.0 + k_factor * dist ** self.params["p1"]
+        return (
+            self.params["ang_0"]
+            * ((1.0 + self.params["step"] / 100.0) ** (handicap + self.params["datum"]))
+            * f_factor
+        )
