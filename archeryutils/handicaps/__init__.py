b4e9fc84b2ff815f3b468c55e7474d367d40494d
Jack Atkinson <jwa34@cam.ac.uk>
2024-02-23 09:41:10 +0000

    Update packaging of handicaps to reflect refactoring changes.

--- archeryutils/handicaps/__init__.py
+++ archeryutils/handicaps/__init__.py
@@ -1,1 +1,21 @@
 """Module providing various handicap functionalities."""
+
+from .handicap_functions import (
+    arrow_score,
+    handicap_from_score,
+    handicap_scheme,
+    score_for_passes,
+    score_for_round,
+)
+from .handicap_scheme import FloatArray
+from .handicap_tables import HandicapTable
+
+__all__ = [
+    "FloatArray",
+    "HandicapTable",
+    "handicap_scheme",
+    "arrow_score",
+    "score_for_passes",
+    "score_for_round",
+    "handicap_from_score",
+]
