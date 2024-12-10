4f5fb06282e5bfb82dcd53179faa9066031c601b
Jack Atkinson <jwa34@cam.ac.uk>
2024-12-10 12:53:29 +0000

    Update old field classification naming to be comsistent with old indoor classification naming.

--- archeryutils/classifications/__init__.py
+++ archeryutils/classifications/__init__.py
@@ -9,8 +9,8 @@ from .agb_indoor_classifications import (
     calculate_agb_indoor_classification,
 )
 from .agb_old_field_classifications import (
-    calculate_old_agb_field_classification,
-    old_agb_field_classification_scores,
+    calculate_agb_old_field_classification,
+    agb_old_field_classification_scores,
 )
 from .agb_old_indoor_classifications import (
     agb_old_indoor_classification_scores,
@@ -30,6 +30,6 @@ __all__ = [
     "agb_old_indoor_classification_scores",
     "agb_field_classification_scores",
     "calculate_agb_field_classification",
-    "calculate_old_agb_field_classification",
-    "old_agb_field_classification_scores",
+    "calculate_agb_old_field_classification",
+    "agb_old_field_classification_scores",
 ]
