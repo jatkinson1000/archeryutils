3a529e7e317f52c57413ed4f02d2827d7801e20a
Marc Tamlyn <marc.tamlyn@gmail.com>
2025-05-11 22:05:05 +0100

    Add calculate_age_group.
    This utility function works out the age group under AGB rules, given a
    year of birth and the year the event takes place in.

---
+++ tests/unit/classifications/test_age_calculator.py
@@ -0,0 +1,38 @@
+"""Tests for age_calculator functions."""
+
+import datetime
+import pytest
+
+from archeryutils.classifications.age_calculator import calculate_age_group
+from archeryutils.classifications.AGB_data import AGB_ages
+
+
+class TestAgeCalculator:
+    """Tests for the age_calculator functions."""
+    def test_adult(self) -> None:
+        year_of_birth = 1989
+        year_of_event = 2025
+
+        assert calculate_age_group(year_of_birth, year_of_event) == AGB_ages.AGE_ADULT
+
+    def test_born_today(self) -> None:
+        # If born today, then will be Under 12. Only safe way to check default year_of_event.
+        assert calculate_age_group(datetime.datetime.now().year) == AGB_ages.AGE_UNDER_12
+
+    def test_juniors(self) -> None:
+        assert calculate_age_group(2014, 2025) == AGB_ages.AGE_UNDER_12
+        assert calculate_age_group(2013, 2025) == AGB_ages.AGE_UNDER_14
+        assert calculate_age_group(2012, 2025) == AGB_ages.AGE_UNDER_14
+        assert calculate_age_group(2011, 2025) == AGB_ages.AGE_UNDER_15
+        assert calculate_age_group(2010, 2025) == AGB_ages.AGE_UNDER_16
+        assert calculate_age_group(2009, 2025) == AGB_ages.AGE_UNDER_18
+        assert calculate_age_group(2008, 2025) == AGB_ages.AGE_UNDER_18
+        assert calculate_age_group(2007, 2025) == AGB_ages.AGE_UNDER_21
+        assert calculate_age_group(2006, 2025) == AGB_ages.AGE_UNDER_21
+        assert calculate_age_group(2005, 2025) == AGB_ages.AGE_UNDER_21
+        assert calculate_age_group(2004, 2025) == AGB_ages.AGE_ADULT
+
+    def test_masters(self) -> None:
+        assert calculate_age_group(1976, 2025) == AGB_ages.AGE_ADULT
+        assert calculate_age_group(1975, 2025) == AGB_ages.AGE_50_PLUS
+        assert calculate_age_group(1950, 2025) == AGB_ages.AGE_50_PLUS
